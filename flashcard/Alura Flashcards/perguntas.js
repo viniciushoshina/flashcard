a28d7ab8b73ca77b5c4d54d29eed98f8d3ca7d2e
viniciushoshina <00001101539215sp@al.educacao.sp.gov.br>
2024-11-26 10:22:55 -0300

    transferindo arquivos

---
+++ flashcard/Alura Flashcards/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    "Programação",
+    "O que é JavaScript?",
+    "O JavaScript é uma linguagem de programação"
+)
+criaCartao(
+    "Programação",
+    "O que é Python?",
+    "O Python é um linguagem de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual é a capital da França?",
+    "A capital da França é Paris"
+)
+
+criaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Uma função é um bloco de código que executa uma tarefa"
+)
+
+criaCartao(
+    "Língua Inglesa",
+    "Como se diz oi em Inglês?",
+    "Oi em Inglês é Hi(RAI)"
+)
